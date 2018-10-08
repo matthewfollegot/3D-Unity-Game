4a4d0b9bc3b138fc19bbdf34620f69c45c60bc00
Matthew Follegot <mfollego@uwaterloo.ca>
2018-10-08 19:33:19 -0400

    Adding player movement via keyboard input

--- FirstGame/Assets/PlayerMovement.cs
+++ FirstGame/Assets/PlayerMovement.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	// Reference to Rigidbody component
+	public Rigidbody rb;
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	// We are naming the method "FixedUpdate" since we are altering the physics
+	void FixedUpdate () {
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if (Input.GetKey("d")) {
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey("a")) {
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+		}
 	}
 }
